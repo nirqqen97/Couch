5192f472773430247a2a621a80e99b7d12d7410b
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2022-09-07 12:30:27 +0300

    тест кнопки

--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,71 @@
-const sliderContainer = document.querySelector('.slider-container')
-const slideRight = document.querySelector('.right-slide')
-const slideLeft = document.querySelector('.left-slide')
-const upButton = document.querySelector('.up-button')
-const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelectorAll('div').length
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const toggle = document.querySelector('#myonoffswitch');
+let auto = true; // Auto scroll
+const intervalTime = 7000;
+let slideInterval;
 
-let activeSlideIndex = 0
-
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
-
-upButton.addEventListener('click', () => changeSlide('up'))
-downButton.addEventListener('click', () => changeSlide('down'))
-
-const changeSlide = (direction) => {
-    const sliderHeight = sliderContainer.clientHeight
-    if(direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex > slidesLength - 1) {
-            activeSlideIndex = 0
-        }
-    } else if(direction === 'down') {
-        activeSlideIndex--
-        if(activeSlideIndex < 0) {
-            activeSlideIndex = slidesLength - 1
-        }
+const nextSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide
+    if (current.nextElementSibling) {
+        // Add current to next sibiling
+        current.nextElementSibling.classList.add('current');
     }
+    else {
+        // Add current to start
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
 
-    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+const prevSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide
+    if (current.previousElementSibling) {
+        // Add current to prev sibiling
+        current.previousElementSibling.classList.add('current');
+    }
+    else {
+        // Add current to last
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+// Button events
+next.addEventListener('click', e => {
+    nextSlide();
+    if(auto) {
+      clearInterval(slideInterval);
+      slideInterval = setInterval(nextSlide, intervalTime);
+    }
+})
+prev.addEventListener('click', e => {
+    prevSlide();
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+})
+
+// Auto slide toggle
+toggle.addEventListener('click', e => {
+    if ( toggle.checked ) {
+      auto = true;
+      slideInterval = setInterval(nextSlide, intervalTime);
+    } else {
+      auto = false;
+      clearInterval(slideInterval);
+    }
+})
+
+// Auto slide
+if(auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
 }
